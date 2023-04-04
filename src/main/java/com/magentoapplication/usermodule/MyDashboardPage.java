38f19744b41abcc4824af6a78a2cf707b7855bdd
abudusataer <m.sallihemin14@gmail.com>
2023-04-05 01:25:00 +0300

    Change Password method was created in MydashboardPage

--- src/main/java/com/magentoapplication/usermodule/MyDashboardPage.java
+++ src/main/java/com/magentoapplication/usermodule/MyDashboardPage.java
@@ -1,5 +1,7 @@
 package com.magentoapplication.usermodule;
 
+import com.magentoapplication.utility.ApplicationConfig;
+import com.magentoapplication.utility.FunctionClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,10 +12,56 @@ public class MyDashboardPage {
 
     CreateAnAccountPage createAnAccountPage;
 
+    FunctionClass functionClass;
+
+    String configFileName="testdatafolder/testdata.properties";
+
     public MyDashboardPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
         createAnAccountPage=new CreateAnAccountPage(driver);
+        functionClass=new FunctionClass(driver);
     }
+
+    @FindBy(xpath = "//a[text()='Account Information']")
+    WebElement myAccountInformationLink;
+
+    @FindBy(css = "#current_password")
+    WebElement currentPasswordField;
+
+    @FindBy(css = "#change_password")
+    WebElement changePasswordCheckbox;
+
+    @FindBy(css = "#password")
+    WebElement newPasswordField;
+
+    @FindBy(css = "#confirmation")
+    WebElement confirmNewPasswordField;
+
+    @FindBy(css = "[title='Save']")
+    WebElement changePasswordSaveButton;
+
+    @FindBy(xpath = "//span[text()='The account information has been saved.']")
+    WebElement verifyChangePassword;
+
+
+    public void changePassword(){
+        functionClass.waitUntilElementPresent(myAccountInformationLink);
+        myAccountInformationLink.click();
+        currentPasswordField.sendKeys(ApplicationConfig.readFromConfigProperties(configFileName,"password"));
+        functionClass.waitUntilElementPresent(changePasswordCheckbox);
+        changePasswordCheckbox.click();
+        newPasswordField.sendKeys(ApplicationConfig.readFromConfigProperties(configFileName,"newPassword"));
+        confirmNewPasswordField.sendKeys(ApplicationConfig.readFromConfigProperties(configFileName,"confirmNewPassword"));
+        changePasswordSaveButton.click();
+    }
+
+    public boolean verifyPasswordChanged(){
+        if (verifyChangePassword.isDisplayed())
+            return true;
+        else return false;
+    }
+
+
 
 }
--- src/test/java/regressionsuite/usertest/FrontEndTestRunner.java
+++ src/test/java/regressionsuite/usertest/FrontEndTestRunner.java
@@ -2,6 +2,7 @@ package regressionsuite.usertest;
 
 import com.magentoapplication.usermodule.CreateAnAccountPage;
 import com.magentoapplication.usermodule.FrontEndLoginPage;
+import com.magentoapplication.usermodule.MyDashboardPage;
 import com.magentoapplication.usermodule.NewsletterSubscriptionPage;
 import com.magentoapplication.utility.TestBase;
 import org.testng.annotations.AfterClass;
@@ -15,12 +16,15 @@ public class FrontEndTestRunner extends TestBase{
 
     CreateAnAccountPage createAnAccountPage;
     NewsletterSubscriptionPage newsletterSubscriptionPage;
+
+    MyDashboardPage myDashboardPage;
     @BeforeClass
     public void setUp() {
         setupBrowserFrontEnd();
         frontEndLoginPage = new FrontEndLoginPage(driver);
         createAnAccountPage=new CreateAnAccountPage(driver);
         newsletterSubscriptionPage=new NewsletterSubscriptionPage(driver);
+        myDashboardPage=new MyDashboardPage(driver);
     }
 
     @Test
@@ -28,13 +32,22 @@ public class FrontEndTestRunner extends TestBase{
 
         frontEndLoginPage.login();
     }
+
     @Test(description = "kaysar",priority =1 )
     public void CreateAnAccount(){
         createAnAccountPage.fillAccountRegistrationForm();
         createAnAccountPage.verifyCreateAnAccountSuccessful();
 
+    }
 
-}
+    @Test
+    public void changePasswordTest(){
+        createAnAccountPage.fillAccountRegistrationForm();
+        //frontEndLoginPage.login();
+        myDashboardPage.changePassword();
+        myDashboardPage.verifyPasswordChanged();
+    }
+
     @Test
     public void clickNewsletterSubscriptionLink(){
         newsletterSubscriptionPage.clickNewsletterSubscriptionLink();
