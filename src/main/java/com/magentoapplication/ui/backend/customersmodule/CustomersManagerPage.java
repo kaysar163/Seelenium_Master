3bf113d27d439c04edc304cdc0ec6e10206f6669
KaysarBakri <kaysar163@hotmail.com>
2023-04-06 20:56:10 +0200

    CustomerPage-addnewCustomer was created

--- src/main/java/com/magentoapplication/ui/backend/customersmodule/CustomersManagerPage.java
+++ src/main/java/com/magentoapplication/ui/backend/customersmodule/CustomersManagerPage.java
@@ -1,11 +1,60 @@
 package com.magentoapplication.ui.backend.customersmodule;
 
+import com.magentoapplication.utility.FunctionClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class CustomersManagerPage {
     WebDriver driver;
-
+    FunctionClass functionClass;
+    String config = "testdatafolder/testdata.properties";
     public CustomersManagerPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
+        functionClass = new FunctionClass(driver);
     }
+
+    @FindBy(xpath = "(//*[contains(text(),'Add New Customer')])[1]")
+    WebElement addNewCustomerLink;
+    @FindBy(id="_accountprefix")
+    WebElement prefixField;
+    @FindBy(id="_accountfirstname")
+    WebElement firstNameField;
+    @FindBy(id="_accountlastname")
+    WebElement lastNameField;
+    @FindBy(id="_accountemail")
+    WebElement emailField;
+    @FindBy(id="_accounttaxvat")
+    WebElement taxNumberField;
+    @FindBy(id="_accountpassword")
+    WebElement passwordfield;
+    @FindBy(xpath = "//*[@class='middle']//button[@title='Save Customer']")
+    WebElement SavaCustomerButton;
+    @FindBy(xpath = "//*[contains(text(),'The customer has been saved.')]")
+    WebElement customerSuccessfullySavadMessage;
+
+    public void addCustomerMethod(String firstName,String lastName,String email,String password){
+        functionClass.waitUntilElementPresent(addNewCustomerLink);
+        addNewCustomerLink.click();
+        functionClass.waitUntilElementPresent(firstNameField);
+        firstNameField.sendKeys(firstName);
+        functionClass.waitUntilElementPresent(lastNameField);
+        lastNameField.sendKeys(lastName);
+        functionClass.waitUntilElementPresent(emailField);
+        emailField.sendKeys(email);
+        functionClass.waitUntilElementPresent(passwordfield);
+        passwordfield.sendKeys(password);
+        functionClass.waitUntilElementPresent(SavaCustomerButton);
+        SavaCustomerButton.click();
+    }
+    public boolean verifyCustomer(){
+        functionClass.waitUntilElementPresent(customerSuccessfullySavadMessage);
+        if (customerSuccessfullySavadMessage.isDisplayed()) {
+            return true;
+        } else return false;
+
+
+}
 }
--- src/test/java/regressionsuite/ui/testngframework/CustomersModuleTestRunner.java
+++ src/test/java/regressionsuite/ui/testngframework/CustomersModuleTestRunner.java
@@ -1,13 +1,23 @@
 package regressionsuite.ui.testngframework;
 
+import com.magentoapplication.ui.backend.customersmodule.CustomersManagerPage;
+import com.magentoapplication.utility.TestBase;
+import org.apache.commons.collections4.sequence.EditScript;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-public class CustomersModuleTestRunner {
+public class CustomersModuleTestRunner extends TestBase {
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
 
     }
+
+    @Test(description= "kaysar", priority = 1)
+    public void ManageraddnewcusstomerTest(){
+        CustomersManagerPage.addCustomerMethod();
+        CustomersManagerPage. verifyCustomer();
+}
     @AfterClass
     public void tearDown(){
 
