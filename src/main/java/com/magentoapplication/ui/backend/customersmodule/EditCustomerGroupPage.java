36f268d233fe12808dfa1a5d772e773db110c0a1
marya <maryamomak45@gmail.com>
2023-04-06 20:39:50 +0200

    edit

--- src/main/java/com/magentoapplication/ui/backend/customersmodule/EditCustomerGroupPage.java
+++ src/main/java/com/magentoapplication/ui/backend/customersmodule/EditCustomerGroupPage.java
@@ -1,15 +1,22 @@
 package com.magentoapplication.ui.backend.customersmodule;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class EditCustomerGroupPage {
     WebDriver driver;
+    String configFileName="testdatafolder/testdata.properties";
 
     public EditCustomerGroupPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
+
     }
+    @FindBy(xpath = "//span[text()='Customer Groups']")
+    WebElement customerGroupsLink;
+
 
 
 
