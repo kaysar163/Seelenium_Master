45b4cf04c0404d7b114c121d88cb328efc99d716
abudusataer <m.sallihemin14@gmail.com>
2023-04-06 22:43:18 +0300

    all pages was updated

--- src/test/java/regressionsuite/ui/testngframework/CustomersModuleTestRunner.java
+++ src/test/java/regressionsuite/ui/testngframework/CustomersModuleTestRunner.java
@@ -1,15 +1,28 @@
 package regressionsuite.ui.testngframework;
 
+import com.magentoapplication.ui.backend.backendlogin.BackEndLogin;
+import com.magentoapplication.utility.TestBase;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
-public class CustomersModuleTestRunner {
+@Listeners(TestListener.class)
+public class CustomersModuleTestRunner extends TestBase {
+
+    BackEndLogin backEndLogin;
+
     @BeforeClass
-    public void setUp(){
-
+    public void setUp(ITestContext context){
+        setupBrowserBackEnd();
+        backEndLogin=new BackEndLogin(driver);
+        backEndLogin.customersModuleLogin();
     }
+
+
+
     @AfterClass
     public void tearDown(){
-
+        closeBrowser();
     }
 }
