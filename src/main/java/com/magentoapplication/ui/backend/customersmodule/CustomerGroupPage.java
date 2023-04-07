479ba7599d8820b8f576c908234d17b881c0b7b3
Fazilet <periyasin@gmail.com>
2023-04-07 13:47:18 +0300

    addnewcustomergroup4

--- src/main/java/com/magentoapplication/ui/backend/customersmodule/CustomerGroupPage.java
+++ src/main/java/com/magentoapplication/ui/backend/customersmodule/CustomerGroupPage.java
@@ -4,14 +4,17 @@ import com.magentoapplication.utility.FunctionClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class CustomerGroupPage {
 
     WebDriver driver;
     FunctionClass functionClass;
-    public CustomerGroupPage(WebDriver driver) {
+        public CustomerGroupPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver,this);
+        functionClass = new FunctionClass(driver);
     }
 
 
