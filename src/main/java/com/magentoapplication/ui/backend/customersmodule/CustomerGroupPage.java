553684ca69ca54e743f9698b15fa972427c01e61
Fazilet <periyasin@gmail.com>
2023-04-07 12:16:19 +0300

    addnewcustomergroup

--- src/main/java/com/magentoapplication/ui/backend/customersmodule/CustomerGroupPage.java
+++ src/main/java/com/magentoapplication/ui/backend/customersmodule/CustomerGroupPage.java
@@ -1,13 +1,15 @@
 package com.magentoapplication.ui.backend.customersmodule;
 
+import com.magentoapplication.utility.FunctionClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class CustomerGroupPage {
 
     WebDriver driver;
-
+    FunctionClass functionClass;
     public CustomerGroupPage(WebDriver driver) {
         this.driver = driver;
     }
@@ -15,6 +17,47 @@ public class CustomerGroupPage {
 
     @FindBy(xpath = "//span[text()='Customer Groups']")
     WebElement customerGroupsLink;
+
+    @FindBy(xpath = "//button[@title='Add New Customer Group']")
+    WebElement AddNewCustomerGroupButton;
+
+    @FindBy(id="customer_group_code")
+    WebElement GroupNameField;
+
+    @FindBy(id="tax_class_id")
+    WebElement TaxClassDropdown;
+
+    @FindBy(css = ".wrapper>.middle>div>.content-header>p>button[title=\"Save Customer Group\"]")
+    WebElement SaveCustomerGroupButton;
+
+    @FindBy(xpath = "//span[contains(text(),'The customer group has been saved.')]")
+    WebElement TheCustomerGroupHasBeenSavedMessage;
+
+
+    public void  AddNewCustomerGroup(){
+        customerGroupsLink.click();
+        functionClass.waitUntilElementPresent(customerGroupsLink);
+        AddNewCustomerGroupButton.click();
+        functionClass.waitUntilElementPresent(AddNewCustomerGroupButton);
+        GroupNameField.clear();
+        GroupNameField.sendKeys(functionClass.generateFakeName());
+        Select select=new Select(TaxClassDropdown);
+        select.selectByValue("5");
+        SaveCustomerGroupButton.click();
+        functionClass.waitUntilElementPresent(SaveCustomerGroupButton);
+
+    }
+
+    public boolean verifyTheCustomerGroupHasBeenSaved() {
+        return TheCustomerGroupHasBeenSavedMessage.isDisplayed();
+
+
+
+
+
+    }
+
+
 
 
 
