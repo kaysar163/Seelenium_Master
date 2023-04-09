41cbbef4471737a518841384f84b36aad2164a50
KaysarBakri <kaysar163@hotmail.com>
2023-04-09 21:09:18 +0200

    AddNewSearchTerm was Update

--- src/main/java/com/magentoapplication/ui/backend/catalogmodule/SearchTermsPage.java
+++ src/main/java/com/magentoapplication/ui/backend/catalogmodule/SearchTermsPage.java
@@ -2,7 +2,10 @@ package com.magentoapplication.ui.backend.catalogmodule;
 
 import com.magentoapplication.utility.FunctionClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class SearchTermsPage {
 
@@ -20,12 +23,67 @@ public class SearchTermsPage {
     }
 
     // Elements
+    @FindBy(xpath = "(//span[contains(text(),'Add New Search Term')])[1]")
+
+    WebElement AddNewSearchTerm;
+    @FindBy( id= "query_text")
+
+    WebElement SearchQuery;
+
+    @FindBy(id = "store_id")
+
+    WebElement Store;
+    @FindBy(id = "synonym_for")
+
+    WebElement SynonymFor;
+    @FindBy(id = "redirect")
+
+    WebElement RedirectURL;
+    @FindBy(id = "display_in_terms")
+
+    WebElement DisplayinSuggestedTerms;
+    @FindBy(xpath = "(//span[contains(text(),'Save Search')])[1]")
+
+    WebElement SaveSearch;
+    @FindBy(xpath = "(//span[normalize-space()='You saved the search term.'])[1]")
+
+    WebElement SuccessfulSavesMessage;
+    public void NewSearchInfo(){
+        catalogDashboardPage. clickOnCatalogLink();
+        catalogDashboardPage.clickOnSearchTerms();
+        functionClass.waitUntilElementPresent(AddNewSearchTerm);
+        AddNewSearchTerm.click();
+        functionClass.waitUntilElementPresent(SearchQuery);
+        SearchQuery.sendKeys(functionClass.generateFakeName());
+        functionClass.waitUntilElementPresent(Store);
+        Select select=new Select(Store);
+        select.selectByValue("28");
+        functionClass.waitUntilElementPresent(SynonymFor);
+        SynonymFor.sendKeys(functionClass.generateFakeName());
+        functionClass.waitUntilElementPresent( RedirectURL);
+        RedirectURL.sendKeys(functionClass.generateFakeRedirectURL());
+        functionClass.waitUntilElementPresent(DisplayinSuggestedTerms);
+        Select select1=new Select(DisplayinSuggestedTerms);
+        select1.selectByValue("0");
+        functionClass.waitUntilElementPresent(SaveSearch);
+        SaveSearch.click();
+    }
+
+    public boolean VerifyAddNewSerach(){
+        functionClass.waitUntilElementPresent(SuccessfulSavesMessage);
+        if(SuccessfulSavesMessage.isDisplayed()){
+            return true;
+        }else return false;
+
+    }
+}
 
 
 
 
 
-    // Methods
+
+
 
 
 
