1749d3f95f2c14cd557181f1c6778eea1e2afcf7
Fazilet <periyasin@gmail.com>
2023-04-10 15:47:54 +0300

    deleteproduct

--- src/main/java/com/magentoapplication/ui/backend/catalogmodule/ProductInfoPage.java
+++ src/main/java/com/magentoapplication/ui/backend/catalogmodule/ProductInfoPage.java
@@ -1,7 +1,11 @@
 package com.magentoapplication.ui.backend.catalogmodule;
 
 import com.magentoapplication.utility.FunctionClass;
+import org.apache.xmlbeans.impl.xb.xsdschema.FieldDocument;
+import org.apache.xmlbeans.impl.xb.xsdschema.SelectorDocument;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ProductInfoPage {
@@ -21,6 +25,19 @@ public class ProductInfoPage {
 
     // Elements
 
+    @FindBy(xpath = "//span[text()='Catalog']")
+    WebElement cataloglink;
+
+    @FindBy(xpath = "//span[text()='Manage Products']")
+    WebElement manageproductslink;
+
+    @FindBy(id="productGrid_product_filter_name")
+    WebElement namefield;
+
+    @FindBy(id="id_13dac701a91cfeac9a4c2d96ac74dc16")
+    WebElement searchbutton;
+
+    @FindBy
 
 
 
