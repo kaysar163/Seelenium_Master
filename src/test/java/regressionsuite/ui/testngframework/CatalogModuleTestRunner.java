490b0a1086112ee852173ad0b129a3d35c0bfc63
mac <omermijit@gmail.com>
2023-04-11 13:49:51 +0300

    delRootCatTest

--- src/test/java/regressionsuite/ui/testngframework/CatalogModuleTestRunner.java
+++ src/test/java/regressionsuite/ui/testngframework/CatalogModuleTestRunner.java
@@ -59,7 +59,7 @@ public class CatalogModuleTestRunner extends TestBase {
         manageCategoriesPage.subCatDelete();
         Assert.assertTrue(manageCategoriesPage.subCatDeleteSuccessful());
     }
-    @Test(description = "Omercan")
+    @Test(description = "Omercan",priority = 3,dependsOnMethods ={"AddRootCategoriesTest"} )
     public void deleteRootCatTes(){
         manageCategoriesPage.deleteRootCat();
         Assert.assertTrue(manageCategoriesPage.deleteCategorySuccessful());
