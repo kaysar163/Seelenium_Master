4e5989d3d314e0988afbece2c729cb8c1f22619f
abudusataer <m.sallihemin14@gmail.com>
2023-04-11 21:03:19 +0300

    Catalog Module Structure

--- src/test/java/regressionsuite/ui/testngframework/StoreModuleTestRunner.java
+++ src/test/java/regressionsuite/ui/testngframework/StoreModuleTestRunner.java
@@ -1,7 +1,7 @@
 package regressionsuite.ui.testngframework;
 
 import com.magentoapplication.ui.backend.backendlogin.BackEndLogin;
-import com.magentoapplication.ui.backend.storemodule.StoreModuleDashboardPage;
+import com.magentoapplication.ui.backend.storemodule.*;
 import com.magentoapplication.utility.TestBase;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,12 +13,28 @@ public class StoreModuleTestRunner extends TestBase {
 
     BackEndLogin backEndLogin;
 
+    ManageCategoriesPage manageCategoriesPage;
+
+    ManageProductsPage manageProductsPage;
+
+    ManageStoresPage manageStoresPage;
+
+    OrdersPage ordersPage;
+
+    OrderViewPage orderViewPage;
+    ProductInfoPage productInfoPage;
 
     @BeforeClass
     public void setUp(){
         setupBrowserBackEnd();
         backEndLogin=new BackEndLogin(driver);
         backEndLogin.storeModuleLogin();
+        manageCategoriesPage=new ManageCategoriesPage(driver);
+        manageProductsPage=new ManageProductsPage(driver);
+        manageStoresPage=new ManageStoresPage(driver);
+        ordersPage=new OrdersPage(driver);
+        orderViewPage=new OrderViewPage(driver);
+        productInfoPage=new ProductInfoPage(driver);
     }
 
 
