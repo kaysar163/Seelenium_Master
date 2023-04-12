e039fb0fd68517df26ee9a9f63abaa660de7623a
m.emin <mamuti@proton.me>
2023-04-12 03:58:32 +0300

    store module product category update

--- src/test/java/regressionsuite/ui/testngframework/StoreModuleTestRunner.java
+++ src/test/java/regressionsuite/ui/testngframework/StoreModuleTestRunner.java
@@ -3,6 +3,7 @@ package regressionsuite.ui.testngframework;
 import com.magentoapplication.ui.backend.backendlogin.BackEndLogin;
 import com.magentoapplication.ui.backend.storemodule.*;
 import com.magentoapplication.utility.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -36,6 +37,12 @@ public class StoreModuleTestRunner extends TestBase {
         orderViewPage=new OrderViewPage(driver);
         productInfoPage=new ProductInfoPage(driver);
     }
+    @Test(description = "Memet")
+    public void updateProductCategoryTest(){
+        productInfoPage.updateProductCategories();
+        Assert.assertTrue(productInfoPage.productCategoryUpdatedSuccessfully());
+    }
+
 
 
 
