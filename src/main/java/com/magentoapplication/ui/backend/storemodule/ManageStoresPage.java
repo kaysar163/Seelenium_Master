805679e2f49c16970006aa66431efc9792b8560d
muradil.dilmurat <muradild@gmail.com>
2023-04-12 20:25:49 +0200

    Add new address

--- src/main/java/com/magentoapplication/ui/backend/storemodule/ManageStoresPage.java
+++ src/main/java/com/magentoapplication/ui/backend/storemodule/ManageStoresPage.java
@@ -27,6 +27,58 @@ public class ManageStoresPage {
     @FindBy(css = "li.success-msg")
     WebElement deleteStoreSuccessMessage;
 
+    @FindBy(xpath = "//span[text()=\"System\"]")
+    WebElement systemLink;
+
+    @FindBy(xpath = "//span[text()=\"Manage Stores\"]")
+    WebElement manageStoresLink;
+
+    @FindBy(xpath = "//span[text()='Create Website']")
+    WebElement createWebsiteButton;
+
+    @FindBy(xpath = "//input[@id=\"website_name\"]")
+    WebElement websiteNameField;
+
+    @FindBy(xpath = "//*[@id='website_code']")
+    WebElement websiteCodeField;
+
+    @FindBy(xpath = "//input[@id=\"website_sort_order\"]")
+    WebElement websiteSortOrderField;
+
+    @FindBy(xpath = "(//span[text()='Save Website'])[1]")
+    WebElement saveWebsiteButton;
+
+    @FindBy(xpath = "//span[text()='The website has been saved.']")
+    WebElement createWebsiteSuccessMassage;
+
+    public void createWebsite(){
+         functionClass.waitUntilElementPresent(systemLink);
+         systemLink.click();
+          functionClass.waitUntilElementPresent(manageStoresLink);
+          manageStoresLink.click();
+        //storeModuleDashboardPage.clickOnManageStoresLink();
+        functionClass.waitUntilElementPresent(createWebsiteButton);
+        createWebsiteButton.click();
+        functionClass.waitUntilElementPresent(websiteNameField);
+        TestHelperClassStore.setWebsiteName(functionClass.generateWebsiteName());
+        websiteNameField.sendKeys(TestHelperClassStore.getWebsiteName());
+        functionClass.waitUntilElementPresent(websiteCodeField);
+        websiteCodeField.sendKeys(functionClass.generateWebsiteCode());
+        functionClass.waitUntilElementPresent(websiteSortOrderField);
+        websiteSortOrderField.sendKeys(functionClass.generateSortOrder());
+        functionClass.waitUntilElementPresent(saveWebsiteButton);
+        saveWebsiteButton.click();
+
+}
+    public boolean verifyCreateWebsite(){
+        functionClass.waitUntilElementPresent(createWebsiteSuccessMassage);
+        if (createWebsiteSuccessMassage.isDisplayed())
+            return true;
+        else return false;
+    }
+
+
+
     //Methods
     public void deleteStore(){
         storeModuleDashboardPage.clickOnManageStoresLink();
