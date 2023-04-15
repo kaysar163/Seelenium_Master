49cfd94ac4e7149b31e6d706e29b7d387aa3c119
Fazilet <periyasin@gmail.com>
2023-04-15 13:17:57 +0300

    testhelperclass1

--- src/main/java/com/magentoapplication/ui/backend/storemodule/TestHelperClassStore.java
+++ src/main/java/com/magentoapplication/ui/backend/storemodule/TestHelperClassStore.java
@@ -2,6 +2,25 @@ package com.magentoapplication.ui.backend.storemodule;
 
 public class TestHelperClassStore {
 
+    private static String productName;
+    private static String storeName;
+    private static String firstname;
+
+    private static String changedProductName;
+    private static String categoryName;
+
+
+    public static String getChangedProductName() {
+        return changedProductName;
+    }
+
+    public static void setChangedProductName(String changedProductName) {
+        TestHelperClassStore.changedProductName = changedProductName;
+
+    }
+
+
+
 
 
 
