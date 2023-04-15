dd92d4cfec68d4a55ec98d64c80d1305750af822
Fazilet <periyasin@gmail.com>
2023-04-15 12:38:58 +0300

    viewstore1

--- src/main/java/com/magentoapplication/ui/backend/storemodule/ManageStoresPage.java
+++ src/main/java/com/magentoapplication/ui/backend/storemodule/ManageStoresPage.java
@@ -3,6 +3,7 @@ package com.magentoapplication.ui.backend.storemodule;
 import com.magentoapplication.utility.FunctionClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -27,6 +28,22 @@ public class ManageStoresPage {
     @FindBy(xpath = "//span[text()='Manage Stores']//parent::a")
     WebElement manageStoresLink;
 
-    @FindBy()
+    @FindBy(xpath = "//*[@title='Id: 559']")
+    WebElement AmesterdamTeam;
 
+
+    public void viewAllStores(){
+      functionClass.waitUntilElementPresent(systemLink);
+        Actions actions=new Actions(driver);
+        actions.moveToElement(systemLink).click(manageStoresLink).build().perform();
+        functionClass.sleep(2);
+    }
+
+    public boolean verifyViewedAllStores(){
+        if (driver.getPageSource().contains("AmsterdamTeam"))
+            return true;
+        else
+            return false;
+
+    }
 }
