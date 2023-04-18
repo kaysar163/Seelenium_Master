46983d06ced3dae31373a6abb9e04b01602ad647
abudusataer <m.sallihemin14@gmail.com>
2023-04-18 23:03:15 +0300

    Cucumber Code Fix

--- src/main/java/com/magentoapplication/ui/backend/salesmodule/ManageCustomersPage.java
+++ src/main/java/com/magentoapplication/ui/backend/salesmodule/ManageCustomersPage.java
@@ -1,7 +1,11 @@
 package com.magentoapplication.ui.backend.salesmodule;
 
+import com.magentoapplication.utility.ApplicationConfig;
 import com.magentoapplication.utility.FunctionClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ManageCustomersPage {
@@ -18,4 +22,33 @@ public class ManageCustomersPage {
         functionClass=new FunctionClass(driver);
         salesDashboardPage=new SalesDashboardPage(driver);
     }
+
+    @FindBy(id = "customerGrid_filter_email")
+    WebElement customerEmailField;
+
+    @FindBy(xpath = "//button[@title='Search']")
+    WebElement searchButton;
+
+    @FindBy(xpath = "//a[text()='Edit']")
+    WebElement editButton;
+
+    @FindBy(id = "(//span[text()='Shopping Cart'])[2]")
+    WebElement shoppingCartField;
+
+    @FindAll(
+            @FindBy(xpath = "//table[@id='customer_cart_grid1_table']//tbody")
+    )
+    WebElement verifyShoppingCartViewed;
+
+    public void viewShoppingCartForCustomers(){
+        salesDashboardPage.clickOnManageCustomersLink();
+        functionClass.waitUntilElementPresent(customerEmailField);
+        customerEmailField.sendKeys(ApplicationConfig.readFromConfigProperties("testdatafolder/testdata.properties","loginEmail"));
+        functionClass.waitUntilElementPresent(searchButton);
+
+    }
+
+
+
+
 }
