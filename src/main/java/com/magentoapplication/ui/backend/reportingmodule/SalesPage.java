15a0cb4e660490d264cdbf0ac6b99256aa0f8b85
KaysarBakri <kaysar163@hotmail.com>
2023-04-28 01:20:03 +0200

    see_Sales_Total_Refunded was created.

--- src/main/java/com/magentoapplication/ui/backend/reportingmodule/SalesPage.java
+++ src/main/java/com/magentoapplication/ui/backend/reportingmodule/SalesPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class SalesPage {
     WebDriver driver;
@@ -31,6 +32,25 @@ public class SalesPage {
     @FindBy(xpath = "//tr[@class='totals']")
     WebElement totalOrder;
 
+    @FindBy(id = "store_switcher")
+    WebElement ShowReportForDropDown;
+    @FindBy(id = "sales_report_report_type")
+    WebElement MatchPeriodToDropDown;
+    @FindBy(id = "sales_report_period_type")
+    WebElement PeriodToDropDown;
+    @FindBy(xpath="(//input[@id='sales_report_from'])[1]")
+    WebElement FromField;
+    @FindBy(id = "sales_report_to")
+    WebElement ToField;
+    @FindBy(id = "sales_report_show_order_statuses")
+    WebElement OrderStatusDropDown;
+    @FindBy(id = "sales_report_show_empty_rows")
+    WebElement EmptyRowsDropDown;
+    @FindBy(xpath = "(//span[contains(text(),'Show Report')])[1]")
+    WebElement ShowReportButton;
+    @FindBy(xpath ="//tfoot//th[2]")
+    WebElement SuccessMessage;
+
     public void viewTotalOrderedReport(String dateFrom,String dateTo){
        reportingDashboardPage.clickOnOrdersLink();
        functionClass.waitUntilElementPresent(fromDateIcon);
@@ -49,6 +69,38 @@ public class SalesPage {
         else return false;
 
     }
+    public void salesTotalRefundedReport(String dateFrom,String dateTo) {
+        reportingDashboardPage.clickOnRefundsLink();
+        functionClass.waitUntilElementPresent(ShowReportForDropDown);
+        Select select=new Select(ShowReportForDropDown);
+        select.selectByValue("341");
+        functionClass.waitUntilElementPresent(MatchPeriodToDropDown);
+        Select select1=new Select(MatchPeriodToDropDown);
+        select1.selectByValue("created_at_order");
+        functionClass.waitUntilElementPresent(PeriodToDropDown);
+        Select select2=new Select(PeriodToDropDown);
+        select2.selectByValue("day");
+        functionClass.waitUntilElementPresent(FromField);
+        FromField.sendKeys(dateFrom);
+        functionClass.waitUntilElementPresent(ToField);
+        ToField.sendKeys(dateTo);
+        functionClass.waitUntilElementPresent(OrderStatusDropDown);
+        Select select3=new Select(OrderStatusDropDown);
+        select3.selectByValue("1");
+        functionClass.waitUntilElementPresent(EmptyRowsDropDown);
+        Select select4=new Select(EmptyRowsDropDown);
+        select4.selectByValue("1");
+        functionClass.waitUntilElementPresent(ShowReportButton);
+        ShowReportButton.click();
+
+    }
+    public boolean verifyRefundedReport(){
+        functionClass.waitUntilElementPresent(SuccessMessage);
+        if (SuccessMessage.isDisplayed()) {
+            return true;
+        } else return false;
+    }
+
 
 
 }
