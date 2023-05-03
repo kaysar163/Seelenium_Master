bb8cee32c72bf32428a2d35ebf43c7301f7ac2b9
mac <omermijit@gmail.com>
2023-05-03 22:20:13 +0300

    database test

--- src/main/java/com/magentoapplication/backend/database/DataAccess.java
+++ src/main/java/com/magentoapplication/backend/database/DataAccess.java
@@ -1,7 +1,66 @@
 package com.magentoapplication.backend.database;
 
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataAccess {
+    public boolean verifyNewProductAddedSuccessfully(String email, Connection connection) {
+        boolean isProductAdded = false;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        CachedRowSet cachedRowSet = null;
 
+        try {
+            cachedRowSet = RowSetProvider.newFactory().createCachedRowSet();
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String selectCatalog = String.format("select * from mg_catalog_category_entity_int where email='%s';", email);
+        try {
+            resultSet = statement.executeQuery(selectCatalog);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        if (resultSet == null) {
+            System.out.println("No records found");
+            return isProductAdded;
+        } else {
+            try {
+                cachedRowSet.populate(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
-
+            String emailAddress = null;
+            int count = 0;
+            while (true) {
+                try {
+                    if (!cachedRowSet.next()) {
+                        break;
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                int entity_id = 0;
+                try {
+                    entity_id = cachedRowSet.getInt("entity_id");
+                    emailAddress = cachedRowSet.getString("email");
+                    System.out.println(String.format("catalog id=%d email=%s", entity_id, emailAddress));
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (count >= 1 & emailAddress.equalsIgnoreCase(email))
+                isProductAdded = true;
+            return isProductAdded;
+        }
+    }
 }
+
+
+
--- src/test/java/regressionsuite/ui/cucumberframework/DatabaseSteps.java
+++ src/test/java/regressionsuite/ui/cucumberframework/DatabaseSteps.java
@@ -1,8 +1,13 @@
 package regressionsuite.ui.cucumberframework;
 
 import com.magentoapplication.backend.database.ConnectionType;
+import com.magentoapplication.backend.database.DataAccess;
 import com.magentoapplication.backend.database.DatabaseConnection;
+import com.magentoapplication.ui.backend.backendlogin.BackEndLogin;
+import com.magentoapplication.ui.backend.catalogmodule.CatalogDashboardPage;
 import com.magentoapplication.utility.ApplicationConfig;
+import com.magentoapplication.utility.FunctionClass;
+import com.magentoapplication.utility.TestBase;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
@@ -11,12 +16,14 @@ import io.cucumber.java.en.When;
 
 import java.sql.Connection;
 
-public class DatabaseSteps {
+public class DatabaseSteps extends TestBase {
 
     Connection connection;
 
     String config="config.properties";
-
+    BackEndLogin backEndLogin;
+    CatalogDashboardPage catalogDashboardPage;
+    FunctionClass functionClass;
 
 
     String dbUrl= ApplicationConfig.readFromConfigProperties(config,"dbIp");
@@ -31,7 +38,6 @@ public class DatabaseSteps {
     @Before("@DatabaseTest")
     public void setUp(){
         connection= DatabaseConnection.connection(dbUrl,dbPort,dbDefault,dbUserName,dbPassword, ConnectionType.MYSQL);
-
     }
 
     @After("@DatabaseTest")
@@ -41,22 +47,33 @@ public class DatabaseSteps {
 
     @Given("connection is already established")
     public void connectionIsAlreadyEstablished() {
+      backEndLogin=new BackEndLogin(driver);
+      catalogDashboardPage=new CatalogDashboardPage(driver);
+      functionClass=new FunctionClass(driver);
+
     }
 
     @When("a new user registers to the system")
     public void aNewUserRegistersToTheSystem() {
+
     }
 
     @Then("the user should be in the database")
     public void theUserShouldBeInTheDatabase() {
+
     }
 
     @When("a new user add product root category")
     public void aNewUserAddProductRootCategory() {
+        backEndLogin.catalogModuleLogin();
+        catalogDashboardPage.clickOnManageCategoriesLink();
+
+
         
     }
 
     @Then("added new product root category should be in the database")
     public void addedNewProductRootCategoryShouldBeInTheDatabase() {
+
     }
 }
