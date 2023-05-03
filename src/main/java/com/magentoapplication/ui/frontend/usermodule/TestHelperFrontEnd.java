19fec9fe38a1e974129e2923c735fdbb2e4c51bf
abudusataer <m.sallihemin14@gmail.com>
2023-05-03 22:42:42 +0300

    Database verify newly registered users should be in the database

---
+++ src/main/java/com/magentoapplication/ui/frontend/usermodule/TestHelperFrontEnd.java
@@ -0,0 +1,13 @@
+package com.magentoapplication.ui.frontend.usermodule;
+
+public class TestHelperFrontEnd {
+    private static String email;
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        TestHelperFrontEnd.email = email;
+    }
+}
